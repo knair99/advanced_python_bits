0ea7158de188dab4e4911f42c542ad451baf5131
unknown <Karunakaran_Prasad@L-254746-P15.SYMC.SYMANTEC.COM>
2016-06-27 14:49:43 -0700

    thread, locks

--- thread.py
+++ thread.py
@@ -1,21 +1,45 @@
 import threading 
 
 
-def audio():
+def audio(audio_event, video_event, lock, name):
 
-	for i in range(0,10):
-		print ("Audio clip, %d \n") % i
+	audio_event.set()
+	video_event.wait()
+	print "Video ready. Audio can start\n"
+
+	for i in range(0,100):
+		lock.acquire()
+		print ("%s Audio clip, %d \n") % (name, i)
+		#print ("%s AAAAAAAAAAAAAAAA %d \n") % (name, i)
+		lock.release()
+
+		
+		
 
 
-def video():
+def video(audio_event, video_event, lock, name):
+	audio_event.wait()
+	video_event.set()
 
-	for i in range(0,10):
-		print ("Video clip, %d \n") % i
+	print "Audio ready. Video can start\n"
+
+	for i in range(0, 100):
+		lock.acquire()
+		print ("%s Video clip, %d \n") % (name, i)
+		#print ("%s VVVVVVVVVVVVVVV %d \n") % (name, i)
+		lock.release()
 
 
 
+#define an event for both threads
+audio_event = threading.Event()
+video_event = threading.Event()
 
-audio_thread = threading.Thread(target=audio)
-video_thread = threading.Thread(target=video)
+lock = threading.Lock()
+
+#first start both threads
+audio_thread = threading.Thread(target=audio, args=(audio_event, video_event, lock, "audiothread >>"))
+video_thread = threading.Thread(target=video, args=(audio_event, video_event, lock, "videothread >>"))
 audio_thread.start()
 video_thread.start()
+
